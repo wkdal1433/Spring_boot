5d78717aafc32d8fe0a2143b7ac0b347f31f0f93
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-02-02 21:25:28 +0900

    aaa
    aaa

--- Spring_boot_workspace/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring_boot_workspace/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,23 +1,37 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
     /** 할인 정책이 바뀌었으니 객체도 바꿔줘야함.
      * 우리가 지금 구현한 것은 의존성을 잘 지킨 것 처럼 보이는데
      * 사실 추상 인터페이스인 OrderServiceImpl뿐만 아니라
      * 아래 FixDiscountPolicy인 구체클래스에도 의존하고 있게 됨.
+     * 그래서 이 의존성을 바꾸는 방법은 추상 클래스만 할당하게 바꾸면 됨.
+     * 그런데 추상클래스만 할당하게 되면 nullpointException이 발생.
+     * 그래서 이걸 주입시켜주는 애가 필요한데 그 역할을 해주는 것이 AppConfig임.
+     *
      * private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+     * private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
      */
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
+
+    /*
+    이 아래처럼 바꾸게 되면 지금 이 OrderServiceImpl 클래스는 완전히 추상클래스에만 의존할 수 있도록 됨.
+    아래 OrderServiceImpl의 매개변수인 discountPolicy로 RateDiscountPolicy가 들어올지 FixDiscountPolicy가
+    들어올지를 이 클래스에서 설정하지 않음으로서 분리됨.
+     */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
     @Override
--- Spring_boot_workspace/core/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ Spring_boot_workspace/core/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,19 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
     @Test
     void join(){//given when then은 어떤 환경이 주어졌는지, 그때 , 무엇을 할 것인지
         //given
