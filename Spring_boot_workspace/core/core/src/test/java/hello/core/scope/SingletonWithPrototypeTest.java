21b0d29e80f5a9421c0abf9ddeb2c73690f43f4a
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-03-06 21:45:47 +0900

    20240306
    finish

--- Spring_boot_workspace/core/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ Spring_boot_workspace/core/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -4,6 +4,8 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -23,7 +25,37 @@ public class SingletonWithPrototypeTest {
 
         ac.close();
     }
+    @Test
+    void singletonClientUsePrototype(){
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
 
+
+    }
+    @Scope("singleton")
+    static class ClientBean{
+//        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+        public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
     @Scope("prototype")
     static class PrototypeBean{
         private int count = 0;
