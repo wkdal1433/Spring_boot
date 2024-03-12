ffec25deec6385bc7df10248639b572cb9dbfaba
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-03-12 18:18:43 +0900

    20240312
    bean scope finish

--- Spring_boot_workspace/core/core/src/main/java/hello/core/web/LogDemoController.java
+++ Spring_boot_workspace/core/core/src/main/java/hello/core/web/LogDemoController.java
@@ -4,6 +4,7 @@ import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+    //이러면 MyLogger가 빈에 주입이 되는 것이 아니라 MyLogger를 찾을 수 있는 Dependency가 주입이 되는 것
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
