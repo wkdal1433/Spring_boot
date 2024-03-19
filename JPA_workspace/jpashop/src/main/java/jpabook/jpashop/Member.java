acdb6147163c2baed4898b596d8d62263a90b4f0
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-03-19 17:45:32 +0900

    2024 03 19
    s

--- JPA_workspace/jpashop/src/main/java/jpabook/jpashop/Member.java
+++ JPA_workspace/jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -1,10 +1,13 @@
 package jpabook.jpashop;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Order;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails.Address;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -12,5 +15,13 @@ public class Member {
     @Id @GeneratedValue
     private Long id;
     private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
 
 }
