75614f4875b80c1fac466c2e2cf6ec99fcff420e
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-03-28 13:45:52 +0900

    2024/03/28 f
    2024/03/28 f

--- JPA_workspace/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ JPA_workspace/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -13,6 +13,10 @@ public class OrderRepository {
     public void save(Order order){
         em.persist(order);
     }
+    public Order findOne(Long id){
+        return em.find(Order.class,id);
+    }
+
 
 
 }
