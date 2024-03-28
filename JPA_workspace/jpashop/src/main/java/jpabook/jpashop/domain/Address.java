ecb3870cb53a572c56da6e2206d9aa27f15fd8e0
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-03-28 19:12:50 +0900

    b
    b

--- JPA_workspace/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ JPA_workspace/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,15 +1,25 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 
 
 }
--- JPA_workspace/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ JPA_workspace/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,10 +1,13 @@
 package jpabook.jpashop.service;
 
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,15 +19,26 @@ import static org.junit.Assert.*;
 @Transactional
 public class OrderServiceTest {
 
+    @Autowired    EntityManager em;
+    @Autowired    OrderService orderService;
+
     @Test
     public void 상품주문() throws Exception {
         //given
         Member member = new Member();
         member.setName("회원1");
-        member.setAddress("");
-        Item item = new Book();
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
 
+        Book book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+
+        int orderCount = 2;
         //when
+        orderService.order(member.getId(), book.getId(), orderCount);
 
         //then
 
