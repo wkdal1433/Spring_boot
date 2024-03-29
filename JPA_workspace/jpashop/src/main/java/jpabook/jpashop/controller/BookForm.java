8480e4e14b60f19baf15004450a8f6ac49d06727
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-03-29 15:37:30 +0900

    b
    b

---
+++ JPA_workspace/jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+    private Long id;
+
+    @NotEmpty(message = "아이템 이름은 필수 입니다.")
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+
+}
