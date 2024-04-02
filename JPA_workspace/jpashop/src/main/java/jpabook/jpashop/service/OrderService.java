d4e3805e1ed8f6fe598b531c9bcc5f73fd53a5bd
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-04-02 20:35:06 +0900

    d
    d

--- JPA_workspace/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ JPA_workspace/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,9 +5,12 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -60,11 +63,11 @@ public class OrderService {
     }
 
     /**주문 검색*/
-/*
+
     public List<Order> findOrders(OrderSearch orderSearch){
-        return orderRepository.findAll(orderSearch);
+        return orderRepository.findAllByString(orderSearch);
     }
-*/
+
 
 
 
