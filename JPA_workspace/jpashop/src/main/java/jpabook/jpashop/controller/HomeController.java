999cd403cf31841c50cf39f8af760483c2593b91
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2024-04-08 20:53:45 +0900

    asd
    asd

--- JPA_workspace/jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
+++ JPA_workspace/jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -15,4 +15,5 @@ public class HomeController {
         log.info("home controller");
         return "home";
     }
+
 }
--- JPA_workspace/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ JPA_workspace/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -10,4 +10,5 @@ import lombok.Setter;
 public class Book extends Item{
     private String author;
     private String isbn;
+
 }
